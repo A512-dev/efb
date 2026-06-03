f9b1d322a8c4cf3ba0b6a0dd74315e39e58efb81
Amirabbas Amiri <amirabbasamiri.dev@gmail.com>
2026-06-03 22:24:52 +0330

    feat: new profile design with flip card

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -248,6 +248,7 @@ export const markMessageAsRead = async (messageId) => {
   const response = await apiClient.post(`/messages/${messageId}/read`);
   return response.data;
 };
+<<<<<<< HEAD
 //updates
 
 export const updateManualPdf = async (manualId, file, { title, note } = {}) => {
@@ -272,3 +273,5 @@ export const getManualUpdates = async () => {
   });
   return response.data;
 };
+=======
+>>>>>>> origin/main
